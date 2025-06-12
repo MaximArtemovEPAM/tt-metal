5545420bda9f6ff4cb43f3c6060c8fed1a4ffd95
Adrian F. Morrison <amorrison@tenstorrent.com>
2025-06-12 21:52:12 +0000

    kernels mostly written

--- tt_metal/fabric/fabric.cpp
+++ tt_metal/fabric/fabric.cpp
@@ -67,14 +67,16 @@ size_t get_tt_fabric_packet_header_size_bytes() {
     return control_plane.get_fabric_context().get_fabric_packet_header_size_bytes();
 }
 
-FabricNodeId get_fabric_node_id_from_physical_chip_id(chip_id_t physical_chip_id) {
-    const auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
-    return control_plane.get_fabric_node_id_from_physical_chip_id(physical_chip_id);
-}
-
 size_t get_tt_fabric_max_payload_size_bytes() {
     const auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
     return control_plane.get_fabric_context().get_fabric_max_payload_size_bytes();
+}
+
+const ControlPlane& get_control_plane() { return tt::tt_metal::MetalContext::instance().get_control_plane(); }
+
+FabricNodeId get_fabric_node_id_from_physical_chip_id(chip_id_t physical_chip_id) {
+    const auto& control_plane = tt::tt_metal::MetalContext::instance().get_control_plane();
+    return control_plane.get_fabric_node_id_from_physical_chip_id(physical_chip_id);
 }
 
 void append_fabric_connection_rt_args(
