20f3d335ffa7026fa622b10114486ec31b932ada
maxim_artemov <maxim_artemov@epam.com>
2025-09-10 03:00:20 -0600

    use GCC 12 for Debian build

--- build_metal_with_conan.sh
+++ build_metal_with_conan.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 CONAN_INSTALL_DIR="$1"
+shift
 
 source $HOME/tt-metal-virtualenv/bin/activate
 source "${CONAN_INSTALL_DIR}/conanbuild.sh"
-./build_metal.sh --conan-install-path "${CONAN_INSTALL_DIR}"
+./build_metal.sh --conan-install-path "${CONAN_INSTALL_DIR}" "$@"
