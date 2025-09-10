87410ab9d1e25e7dfc102fb974c5354f4852a74a
maxim_artemov <maxim_artemov@epam.com>
2025-09-10 04:49:02 -0600

    rhel build with conan

--- build_metal_in_docker.sh
+++ build_metal_in_docker.sh
@@ -4,26 +4,12 @@ set -o nounset
 set -x
 
 DISTRO="$1"
-COMPILER="$2"
 
 case "$DISTRO" in
     "debian"|"fedora"|"rhel"|"ubuntu")
         ;;
     *)
         echo "Expected DISTRO as debian/fedora/rhel/ubuntu, got '${DISTRO}'"
-        exit 1
-        ;;
-esac
-
-case "$COMPILER" in
-    "clang_17")
-        TOOLCHAIN_PATH="cmake/x86_64-linux-clang-17-libstdcpp-toolchain.cmake"
-        ;;
-    "gcc_12")
-        TOOLCHAIN_PATH="cmake/x86_64-linux-gcc-12-toolchain.cmake"
-        ;;
-    *)
-        echo "Error: COMPILER must be 'clang_17' or 'gcc_12', got: '$COMPILER'"
         exit 1
         ;;
 esac
@@ -39,4 +25,4 @@ docker build --progress=plain \
     -f dockerfile/Dockerfile.conan-test.${DISTRO} -t "${IMAGE_NAME}" .
 
 docker run -it --rm -v $(pwd):/workspace "${IMAGE_NAME}" \
-    ./build_metal_with_conan.sh /tt-metal-conan/conan-build --toolchain-path "${TOOLCHAIN_PATH}"
+    ./build_metal_with_conan.sh /tt-metal-conan/conan-build "${DISTRO}"
