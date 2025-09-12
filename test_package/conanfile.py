3c379c04872a3c16a481c9a6fe540e5dbe4bdfd9
maxim_artemov <maxim_artemov@epam.com>
2025-09-12 05:52:29 -0600

    conan create works in debian

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -11,25 +11,40 @@ import os
 
 class TestPackageConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeDeps"
     test_type = "explicit"
 
     def requirements(self):
         self.requires(self.tested_reference_str)
+        self.requires("openmpi/4.1.6")
+        self.requires("capstone/5.0.6")
+        self.requires("libnuma/2.0.19")
+        self.requires("boost/1.88.0")
+        self.requires("hwloc/2.10.0")
 
     def layout(self):
         cmake_layout(self)
 
     def build(self):
         cmake = CMake(self)
-        cmake.configure()
+        cmake.configure(cli_args=["--graphviz=build-visual.dot"])
         cmake.build()
 
+    def build_requirements(self):
+        self.tool_requires("ninja/[>=1.11.1]")
+        self.tool_requires("cmake/[>=3.25]")
+
     def generate(self):
+        from conan.tools.cmake import CMakeToolchain
+
+        tc = CMakeToolchain(self)
+        tc.generator = "Ninja"
+        tc.generate()
+
         runenv = VirtualRunEnv(self)
         runenv.generate()
 
     def test(self):
         if can_run(self):
             bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
-            self.run(bin_path, env="conanrun")
+            # self.run(bin_path, env="conanrun")
